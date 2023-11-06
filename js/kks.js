39c86381c949b3c7dc9eeac37c6daca2a7051c2a
Greg Miller <hello@gregmiller.io>
2023-11-05 20:12:53 -0500

    Create kks.js
    Add 'is-position-sticky' class
    - setting not available in Site Editor

---
+++ js/kks.js
@@ -0,0 +1,6 @@
+/* Add sticky header class
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+ * .is-position-sticky is disabled in mobile view
+ */
+const siteHeader = document.querySelector('.site-header');
+siteHeader.classList.add('is-position-sticky');
