4da45e866b36e005d57a97f7a3cd0abfb446b9fc
Greg Miller <hello@gregmiller.io>
2024-09-11 10:10:49 -0400

    Update kks.js
    Minor refactoring

--- js/kks.js
+++ js/kks.js
@@ -2,41 +2,41 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
  * .is-position-sticky is disabled in mobile view
  */
-const siteHeader = document.querySelector('.site-header');
-siteHeader.classList.add('is-position-sticky');
+const siteHeader = document.querySelector('.site-header')
+const siteLogos = document.querySelectorAll('.site--logo')
 
-const siteLogos = document.querySelectorAll('.site--logo');
+siteHeader.classList.add('is-position-sticky')
 
 siteLogos.forEach(function (siteLogo) {
     // Check if siteLogo actually has a parentNode
     if (siteLogo.parentNode) {
         // Create a new anchor element
-        const anchor = document.createElement('a');
+        const anchor = document.createElement('a')
         anchor.href = '/';
-        anchor.title = 'Kool Kat Science - Home';
+        anchor.title = 'Kool Kat Science - Home'
 
         // Copy all classes from siteLogo to anchor
-        anchor.className = siteLogo.className;
+        anchor.className = siteLogo.className
 
         // Create a new span element
-        const span = document.createElement('span');
+        const span = document.createElement('span')
 
         // Set the class name of the span to 'hidden'
-        span.className = 'visually-hidden';
+        span.className = 'visually-hidden'
 
         // Add text to the span
-        span.textContent = 'Kool Kat Science'; // Replace with your desired text
+        span.textContent = 'Kool Kat Science' // Replace with your desired text
 
         // Append the span to the anchor
-        anchor.appendChild(span);
+        anchor.appendChild(span)
 
         // Replace siteLogo with the new anchor element
-        siteLogo.parentNode.replaceChild(anchor, siteLogo);
+        siteLogo.parentNode.replaceChild(anchor, siteLogo)
     } else {
-        console.log('Parent node not found for:', siteLogo);
+        console.log('Parent node not found for:', siteLogo)
     }
 });
 
 // make the label of the search field invisible
-const searchLabel = document.querySelector('.wp-block-search__label');
-searchLabel.classList.add('visually-hidden');
+const searchLabel = document.querySelector('.wp-block-search__label')
+searchLabel.classList.add('visually-hidden')
